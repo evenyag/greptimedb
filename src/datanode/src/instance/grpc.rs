ecadb4bb1a86c20cf6e541fc44279d4fc21f7bb2
evenyag <realevenyag@gmail.com>
2023-06-03 16:23:12 +0800

    feat: don't spawn task to write table

--- src/datanode/src/instance/grpc.rs
+++ src/datanode/src/instance/grpc.rs
@@ -28,7 +28,6 @@ use datafusion::catalog::catalog::{
 };
 use datafusion::catalog::schema::SchemaProvider;
 use datafusion::datasource::TableProvider;
-use futures::future;
 use query::parser::{PromQuery, QueryLanguageParser, QueryStatement};
 use query::plan::LogicalPlan;
 use query::query_engine::SqlStatementExecutor;
@@ -44,7 +43,7 @@ use table::table::adapter::DfTableProviderAdapter;
 use crate::error::{
     self, CatalogNotFoundSnafu, CatalogSnafu, DecodeLogicalPlanSnafu, DeleteExprToRequestSnafu,
     DeleteSnafu, ExecuteLogicalPlanSnafu, ExecuteSqlSnafu, InsertDataSnafu, InsertSnafu,
-    JoinTaskSnafu, PlanStatementSnafu, Result, SchemaNotFoundSnafu, TableNotFoundSnafu,
+    PlanStatementSnafu, Result, SchemaNotFoundSnafu, TableNotFoundSnafu,
 };
 use crate::instance::Instance;
 
@@ -126,36 +125,31 @@ impl Instance {
         requests: InsertRequests,
         ctx: &QueryContextRef,
     ) -> Result<Output> {
-        let results = future::try_join_all(requests.inserts.into_iter().map(|insert| {
-            let catalog_manager = self.catalog_manager.clone();
-            let catalog = ctx.current_catalog();
-            let schema = ctx.current_schema();
+        let catalog = ctx.current_catalog();
+        let schema = ctx.current_schema();
+        let mut affected_rows = 0;
 
-            common_runtime::spawn_write(async move {
-                let table_name = &insert.table_name.clone();
-                let table = catalog_manager
-                    .table(&catalog, &schema, table_name)
-                    .await
-                    .context(CatalogSnafu)?
-                    .with_context(|| TableNotFoundSnafu {
-                        table_name: common_catalog::format_full_table_name(
-                            &catalog, &schema, table_name,
-                        ),
-                    })?;
-
-                let request =
-                    to_table_insert_request(&catalog, &schema, insert).context(InsertDataSnafu)?;
-
-                table.insert(request).await.with_context(|_| InsertSnafu {
+        for insert in requests.inserts {
+            let table_name = &insert.table_name.clone();
+            let table = self
+                .catalog_manager
+                .table(&catalog, &schema, table_name)
+                .await
+                .context(CatalogSnafu)?
+                .with_context(|| TableNotFoundSnafu {
                     table_name: common_catalog::format_full_table_name(
                         &catalog, &schema, table_name,
                     ),
-                })
-            })
-        }))
-        .await
-        .context(JoinTaskSnafu)?;
-        let affected_rows = results.into_iter().sum::<Result<usize>>()?;
+                })?;
+
+            let request =
+                to_table_insert_request(&catalog, &schema, insert).context(InsertDataSnafu)?;
+
+            affected_rows += table.insert(request).await.with_context(|_| InsertSnafu {
+                table_name: common_catalog::format_full_table_name(&catalog, &schema, table_name),
+            })?;
+        }
+
         Ok(Output::AffectedRows(affected_rows))
     }
 
