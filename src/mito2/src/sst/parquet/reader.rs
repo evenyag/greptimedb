2bf32f83086cb3e89ddf1921185abb15c8d7d843
evenyag <realevenyag@gmail.com>
2023-11-06 21:19:07 +0800

    feat: merge metrics

--- src/mito2/src/sst/parquet/reader.rs
+++ src/mito2/src/sst/parquet/reader.rs
@@ -351,6 +351,7 @@ pub struct ParquetReader {
     current_reader: Option<ParquetRecordBatchReader>,
     /// Buffered batches to return.
     batches: VecDeque<Batch>,
+    /// Local metrics.
     metrics: Metrics,
 }
 
