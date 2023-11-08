deb098e8ddc96c60d112aefcd80614a328766609
evenyag <realevenyag@gmail.com>
2023-11-08 11:35:34 +0800

    feat: rollback compression and page size

--- src/mito2/src/sst/parquet/writer.rs
+++ src/mito2/src/sst/parquet/writer.rs
@@ -17,8 +17,7 @@
 use common_telemetry::debug;
 use common_time::Timestamp;
 use object_store::ObjectStore;
-use parquet::basic::{Compression, Encoding};
-// use parquet::basic::{Compression, Encoding, ZstdLevel};
+use parquet::basic::{Compression, Encoding, ZstdLevel};
 use parquet::file::metadata::KeyValue;
 use parquet::file::properties::WriterProperties;
 use parquet::schema::types::ColumnPath;
@@ -70,12 +69,9 @@ impl ParquetWriter {
         // TODO(yingwen): Find and set proper column encoding for internal columns: op type and tsid.
         let props_builder = WriterProperties::builder()
             .set_key_value_metadata(Some(vec![key_value_meta]))
-            // .set_compression(Compression::ZSTD(ZstdLevel::default()))
-            .set_compression(Compression::LZ4)
+            .set_compression(Compression::ZSTD(ZstdLevel::default()))
             .set_encoding(Encoding::PLAIN)
             .set_max_row_group_size(opts.row_group_size)
-            // Data page size limit.
-            .set_data_page_size_limit(1024 * 64)
             .set_column_encoding(
                 ColumnPath::new(vec![SEQUENCE_COLUMN_NAME.to_string()]),
                 Encoding::DELTA_BINARY_PACKED,
