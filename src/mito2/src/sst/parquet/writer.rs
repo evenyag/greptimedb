c487337eb2a3919d9e88876ce9d99356ffb7cbd7
evenyag <realevenyag@gmail.com>
2023-11-10 14:29:46 +0800

    feat: 1m page

--- src/mito2/src/sst/parquet/writer.rs
+++ src/mito2/src/sst/parquet/writer.rs
@@ -76,7 +76,7 @@ impl ParquetWriter {
             .set_key_value_metadata(Some(vec![key_value_meta]))
             .set_compression(Compression::LZ4)
             // .set_compression(Compression::ZSTD(ZstdLevel::default()))
-            .set_data_page_size_limit(1024 * 128)
+            // .set_data_page_size_limit(1024 * 128)
             .set_encoding(Encoding::PLAIN)
             .set_max_row_group_size(opts.row_group_size)
             .set_column_encoding(
