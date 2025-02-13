ee96d73b44892ee5dae41732cd48b6eb794dc9e1
evenyag <realevenyag@gmail.com>
2025-02-13 15:00:34 +0800

    feat: run pending task in put_dir

--- src/puffin/src/puffin_manager/stager/bounded_stager.rs
+++ src/puffin/src/puffin_manager/stager/bounded_stager.rs
@@ -237,7 +237,13 @@ impl Stager for BoundedStager {
             })
             .await
             .map(|_| ())
-            .context(CacheGetSnafu)
+            .context(CacheGetSnafu)?;
+
+        // Dir is usually large.
+        // Runs pending tasks of the cache and recycle bin to free up space
+        // more quickly.
+        self.cache.run_pending_tasks().await;
+        self.recycle_bin.run_pending_tasks().await;
     }
 }
 
