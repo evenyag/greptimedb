00a26729520f05d83fdc998d02b5e05d61b2b9f5
evenyag <realevenyag@gmail.com>
2025-03-04 14:34:40 +0800

    fix: revert multiple pools which use too much memory

--- src/api/src/lib.rs
+++ src/api/src/lib.rs
@@ -36,36 +36,22 @@ pub mod pool {
     use crate::v1::Rows;
 
     pub struct RowsPool {
-        /// Pools for rows len:
-        /// `<16, <64, <256, >=256
-        pools: [Pool<Rows>; 4],
+        pools: Pool<Rows>,
     }
 
     impl RowsPool {
         pub fn new(capacity: usize) -> Self {
-            let pools = std::array::from_fn(|_| Pool::new(capacity, Rows::default));
+            let pools = Pool::new(capacity, Rows::default);
             Self { pools }
         }
 
-        pub fn try_pull(&self, expect_size: usize) -> Option<Rows> {
-            let row_opt = match expect_size {
-                0..16 => self.pools[0].try_pull(),
-                16..64 => self.pools[1].try_pull(),
-                64..256 => self.pools[2].try_pull(),
-                _ => self.pools[3].try_pull(),
-            };
-
+        pub fn try_pull(&self) -> Option<Rows> {
+            let row_opt = self.pools.try_pull();
             row_opt.map(|row| row.detach().1)
         }
 
         pub fn attach(&self, rows: Rows) {
-            let len = rows.rows.len();
-            match len {
-                0..16 => self.pools[0].attach(rows),
-                16..64 => self.pools[1].attach(rows),
-                64..256 => self.pools[2].attach(rows),
-                _ => self.pools[3].attach(rows),
-            };
+            self.pools.attach(rows)
         }
     }
 
