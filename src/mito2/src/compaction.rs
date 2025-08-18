aa0d66ca3c0b90f7d71234cb1d619bbd548b10cc
evenyag <realevenyag@gmail.com>
2025-08-18 19:57:28 +0800

    fix: set flat format to true in build_flat_sst_reader()
    Signed-off-by: evenyag <realevenyag@gmail.com>

--- src/mito2/src/compaction.rs
+++ src/mito2/src/compaction.rs
@@ -665,19 +665,17 @@ impl CompactionSstReaderBuilder<'_> {
 
     /// Builds [BoxedRecordBatchStream] that reads all SST files and yields batches in flat format for compaction.
     async fn build_flat_sst_reader(self) -> Result<BoxedRecordBatchStream> {
-        let mut scan_input = ScanInput::new(
-            self.sst_layer,
-            ProjectionMapper::all(&self.metadata, false)?,
-        )
-        .with_files(self.inputs.to_vec())
-        .with_append_mode(self.append_mode)
-        // We use special cache strategy for compaction.
-        .with_cache(CacheStrategy::Compaction(self.cache))
-        .with_filter_deleted(self.filter_deleted)
-        // We ignore file not found error during compaction.
-        .with_ignore_file_not_found(true)
-        .with_merge_mode(self.merge_mode)
-        .with_flat_format(true);
+        let mut scan_input =
+            ScanInput::new(self.sst_layer, ProjectionMapper::all(&self.metadata, true)?)
+                .with_files(self.inputs.to_vec())
+                .with_append_mode(self.append_mode)
+                // We use special cache strategy for compaction.
+                .with_cache(CacheStrategy::Compaction(self.cache))
+                .with_filter_deleted(self.filter_deleted)
+                // We ignore file not found error during compaction.
+                .with_ignore_file_not_found(true)
+                .with_merge_mode(self.merge_mode)
+                .with_flat_format(true);
 
         // This serves as a workaround of https://github.com/GreptimeTeam/greptimedb/issues/3944
         // by converting time ranges into predicate.
