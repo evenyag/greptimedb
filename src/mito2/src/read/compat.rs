6b78355da1c1542c814cda1509b12f35deaec059
evenyag <realevenyag@gmail.com>
2025-08-20 20:11:57 +0800

    chore: ensure len >= 1
    Signed-off-by: evenyag <realevenyag@gmail.com>

--- src/mito2/src/read/compat.rs
+++ src/mito2/src/read/compat.rs
@@ -719,8 +719,9 @@ impl FlatRewritePrimaryKey {
         );
 
         // Binary buffer for the primary key.
-        let mut buffer =
-            Vec::with_capacity(old_pk_values_array.value_data().len() / old_pk_values_array.len());
+        let mut buffer = Vec::with_capacity(
+            old_pk_values_array.value_data().len() / old_pk_values_array.len().max(1),
+        );
         // Iterates the binary array and rewrites the primary key.
         for value in old_pk_values_array.iter() {
             let Some(old_pk) = value else {
@@ -869,7 +870,7 @@ impl FlatCompatPrimaryKey {
 
         // Binary buffer for the primary key.
         let mut buffer = Vec::with_capacity(
-            old_pk_values_array.value_data().len() / old_pk_values_array.len()
+            old_pk_values_array.value_data().len() / old_pk_values_array.len().max(1)
                 + converter.estimated_size().unwrap_or_default(),
         );
 
