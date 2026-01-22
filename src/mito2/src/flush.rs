de0e02cfefa8db49a6b99402f7a8b10e7e513a39
evenyag <realevenyag@gmail.com>
2026-01-22 14:37:04 +0800

    feat: add flush file number metrics
    Signed-off-by: evenyag <realevenyag@gmail.com>

--- src/mito2/src/flush.rs
+++ src/mito2/src/flush.rs
@@ -44,7 +44,7 @@ use crate::memtable::{
     BoxedRecordBatchIterator, EncodedRange, IterBuilder, MemtableRanges, RangesOptions,
 };
 use crate::metrics::{
-    FLUSH_BYTES_TOTAL, FLUSH_ELAPSED, FLUSH_FAILURE_TOTAL, FLUSH_REQUESTS_TOTAL,
+    FLUSH_BYTES_TOTAL, FLUSH_ELAPSED, FLUSH_FAILURE_TOTAL, FLUSH_FILE_TOTAL, FLUSH_REQUESTS_TOTAL,
     INFLIGHT_FLUSH_COUNT,
 };
 use crate::read::dedup::{DedupReader, LastNonNull, LastRow};
@@ -538,6 +538,7 @@ impl RegionFlushTask {
                     .access_layer
                     .write_sst(write_request, &write_opts, &mut metrics)
                     .await?;
+                FLUSH_FILE_TOTAL.inc_by(ssts_written.len() as u64);
                 if ssts_written.is_empty() {
                     // No data written.
                     continue;
@@ -600,6 +601,7 @@ impl RegionFlushTask {
                 let ssts = access_layer
                     .write_sst(write_request, &write_opts, &mut metrics)
                     .await?;
+                FLUSH_FILE_TOTAL.inc_by(ssts.len() as u64);
                 Ok((max_sequence, ssts, metrics))
             });
             tasks.push(task);
@@ -614,6 +616,7 @@ impl RegionFlushTask {
                 let metrics = access_layer
                     .put_sst(&encoded.data, region_id, &encoded.sst_info, &cache_manager)
                     .await?;
+                FLUSH_FILE_TOTAL.inc();
                 Ok((max_sequence, smallvec![encoded.sst_info], metrics))
             });
             tasks.push(task);
